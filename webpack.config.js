0cfade3655961a9852d4d1b171f6352200a10531
Joshua Chin <joshuarchin@gmail.com>
2018-07-28 09:34:34 -0400

    added plugins

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({title: 'From Scratch'}),
+    new CleanWebpackPlugin(['dist'])
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
