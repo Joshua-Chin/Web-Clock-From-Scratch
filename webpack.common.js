ed4f8b1aaf689687e83849a1610530c5f7261748
Joshua Chin <joshuarchin@gmail.com>
2018-07-28 20:14:54 -0400

    changed formatting

--- webpack.common.js
+++ webpack.common.js
@@ -15,9 +15,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
+          options: {presets: ['env']}
         }
       }
     ]
