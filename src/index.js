a0102fdbd3d9524e00ff4f0cb0df30269d6313df
Joshua Chin <joshuarchin@gmail.com>
2018-07-31 10:43:02 -0400

    added missing semicolon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const component = () => {
   const element = document.createElement('p');
   element.innerHTML = ['Hello', 'Webpack'].join(' ');
   return element;
-}
+};
 
 document.body.appendChild(component());
