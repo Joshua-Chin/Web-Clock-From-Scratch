b101abca08bda4d8f35f481777a9daf0bef06682
Joshua Chin <joshuarchin@gmail.com>
2018-08-03 11:18:20 -0400

    improved test

--- client/src/message.test.js
+++ client/src/message.test.js
@@ -2,9 +2,11 @@ import message from './message';
 
 global.fetch = jest.fn()
   .mockImplementation(
-    () => Promise.resolve({json: () => "today"})
+    () => Promise.resolve({
+      json: () => Promise.resolve({time: 'today'})
+    })
   );
 
 test('message', async () => {
-  expect(await message()).toMatch('Hello! The current time is');
+  expect(await message()).toMatch('Hello! The current time is today');
 });
